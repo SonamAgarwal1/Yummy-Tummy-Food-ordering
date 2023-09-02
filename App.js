fb52f1b030b1a1c6bdd95cfbc33c8492cc22a959
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-09-02 21:31:14 +0530

    createElemenet and JSX renders in same way

--- App.js
+++ App.js
@@ -1,2 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Namaste");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const jsxheading = <h1>Namaste Jee</h1>;
+root.render(jsxheading);
