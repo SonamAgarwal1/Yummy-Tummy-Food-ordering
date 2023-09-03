31e38389beea43e2fba447054b77d6ce183a0b03
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-09-03 23:54:38 +0530

    bracket for react element if more than 1 line

--- App.js
+++ App.js
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {}, "Namaste");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const jsxheading = <h1>Namaste Jee</h1>;
+const jsxheading = (
+  <h1 className="head" tabIndex="5">
+    Namaste Jee
+  </h1>
+);
 root.render(jsxheading);
