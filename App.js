866e88a9b4df440dc3469a22bfadf55e377e80bb
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-09-04 00:44:49 +0530

    component composition

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {}, "Namaste");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const jsxheading = (
+
+const Jsxheading = () => (
   <h1 className="head" tabIndex="5">
     Namaste Jee
   </h1>
 );
-root.render(jsxheading);
+
+//Component compositioning- using one component within another
+const HeadingComponent = () => {
+  return (
+    <div className="heading">
+      <Jsxheading />
+      <h1>Namaste react functional component</h1>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
