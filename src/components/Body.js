7a8cf1c059d9529d5737eda2e4d1b0ecaa2ca71c
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-09-28 19:39:02 +0530

    adding filter class

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,14 +4,17 @@ import { resList } from "../utils/mockData";
 const Body = () => {
   return (
     <div className="res-body">
-      <button
-        className="top-rated-btn"
-        onClick={() => {
-          console.log("button clicked");
-        }}
-      >
-        Top Rated Resturants
-      </button>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("button clicked");
+          }}
+        >
+          Top Rated Resturants
+        </button>
+      </div>
+
       <div className="res-container">
         {resList.map((resturant) => (
           <ResturantCard resObj={resturant} key={resturant?.info?.id} />
