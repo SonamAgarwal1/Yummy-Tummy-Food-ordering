9feedda7e10d3f23b308deaabeda1a122ca3517b
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-11 00:35:53 +0530

    useRouteError from router-dom

---
+++ src/components/Error.js
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <>
+      Errorrrr!
+      <h3>
+        {err.status}:{err.statusText}
+      </h3>
+    </>
+  );
+};
+export default Error;
