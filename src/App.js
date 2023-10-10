7f0d79cb7ad62e2184a84248cbcd2e467972a8aa
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-10 21:58:25 +0530

    routing configuration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
 
 const AppLayout = () => {
   return (
@@ -13,5 +14,10 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  { path: "/", element: <AppLayout /> },
+  { path: "/about", element: <About /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
