0904c0dba069e6fb314a67b3a2ee3b069363daff
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-17 23:40:42 +0530

    static resturant menu page

---
+++ src/components/ResturantMenu.js
@@ -0,0 +1,15 @@
+const ResturantMenu = () => {
+  return (
+    <div className="menu">
+      <h1>Name of the restaurants</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Burger</li>
+        <li>Pizza</li>
+        <li>Coke</li>
+        <li>Biryani</li>
+        <li>Puri bhaji</li>
+      </ul>
+    </div>
+  );
+};
