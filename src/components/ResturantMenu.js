243a15c900e6c9ee99e1105b2ce269b733726c03
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-20 00:04:28 +0530

    adding ruppess details in the rest menu

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -34,7 +34,9 @@ const ResturantMenu = () => {
       <h2>Menu</h2>
       <ul>
         {itemCards?.map((res) => (
-          <li>{res.card.info.name}</li>
+          <li key={res.card.info.id}>
+            {res.card.info.name}- Rs. {res.card.info.price / 100}
+          </li>
         ))}
       </ul>
     </div>
