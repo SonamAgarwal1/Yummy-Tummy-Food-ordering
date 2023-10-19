5d7e6c9782eb6a91203511a42fc851db17802a0c
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-19 23:54:13 +0530

    adding res menu with dynamic routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
+import ResturantMenu from "./components/ResturantMenu";
 
 const AppLayout = () => {
   return (
@@ -24,6 +25,7 @@ const appRouter = createBrowserRouter([
       { path: "/", element: <Body /> },
       { path: "/about", element: <About /> },
       { path: "/contact", element: <Contact /> },
+      { path: "/resturants/:resId", element: <ResturantMenu /> },
     ],
     errorElement: <Error />,
   },
--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -1,15 +1,44 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const ResturantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6093909&lng=73.7182088&restaurantId=237669&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json);
+    console.log(
+      json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
+        .itemCards
+    );
+  };
+  if (resInfo === null) return <Shimmer />;
+
+  const { itemCards } =
+    resInfo.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
+
   return (
     <div className="menu">
-      <h1>Name of the restaurants</h1>
+      <h1>{resInfo?.data?.cards[0]?.card?.card?.info?.name}</h1>
+      <p>
+        {resInfo?.data?.cards[0]?.card?.card?.info?.cuisines?.join(", ")}-
+        {resInfo?.data?.cards[0]?.card?.card?.info?.costForTwoMessage}
+      </p>
       <h2>Menu</h2>
       <ul>
-        <li>Burger</li>
-        <li>Pizza</li>
-        <li>Coke</li>
-        <li>Biryani</li>
-        <li>Puri bhaji</li>
+        {itemCards?.map((res) => (
+          <li>{res.card.info.name}</li>
+        ))}
       </ul>
     </div>
   );
 };
+
+export default ResturantMenu;
