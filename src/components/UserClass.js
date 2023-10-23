019ffb4c0d7fb5079a730f271f71a52a709b09ac
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-23 21:31:43 +0530

    Declaring state variables in class components and destructuring on the go

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,12 +3,19 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+      count1: 1,
+    };
   }
 
   render() {
     const { name } = this.props; // destructuring props
+    const { count, count1 } = this.state;
     return (
       <div className="user-card">
+        <p>Count:{count}</p>
+        <p>Count2:{count1}</p>
         <h2>Name: {name}</h2>
         <h3>Location: Pune</h3>
         <h4>Contact: @sonamagarwal12</h4>
