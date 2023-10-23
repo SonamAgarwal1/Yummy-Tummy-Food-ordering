cc75631443f4a9f8be8346e5bf8a5ac3d0bd5008
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-23 21:26:39 +0530

    destructuring props in class based component-userClass.js

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,12 +3,13 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
   }
+
   render() {
+    const { name } = this.props; // destructuring props
     return (
       <div className="user-card">
-        <h2>Name: {this.props.name}</h2>
+        <h2>Name: {name}</h2>
         <h3>Location: Pune</h3>
         <h4>Contact: @sonamagarwal12</h4>
       </div>
