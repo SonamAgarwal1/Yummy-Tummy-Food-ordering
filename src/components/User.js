85b33fd286b3857c5249b05f667e2999bca6a6b2
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2023-10-23 21:30:56 +0530

    Declaring state variables in functional components

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
+      <p>Count:{count}</p>
+      <p>Count2:{count2}</p>
       <h2>Name: {name}</h2>
       <h3>Location: Pune</h3>
       <h4>Contact: @sonamagarwal12</h4>
