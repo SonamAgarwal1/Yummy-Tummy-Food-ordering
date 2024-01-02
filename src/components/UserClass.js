c676ddbb7ee8b6f91f7585635f3be7112716d6e2
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-02 20:53:44 +0530

    removing unwanted code from User class compoennt

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,25 +3,12 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-      count1: 1,
-    };
   }
 
   render() {
     const { name } = this.props; // destructuring props
-    const { count, count1 } = this.state;
     return (
       <div className="user-card">
-        <p>Count:{count}</p>
-        <button
-          onClick={() => {
-            this.setState({ count: this.state.count + 1 });
-          }}
-        >
-          Increase Count
-        </button>
         <h2>Name: {name}</h2>
         <h3>Location: Pune</h3>
         <h4>Contact: @sonamagarwal12</h4>
