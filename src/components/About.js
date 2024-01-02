cdceb33c89714ae9882f44b3b1e6f83646279dc7
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-02 21:22:11 +0530

    Converting About component from function to Class based

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <>
-      <UserClass name="Sonam Agarwal(Class)" />
-    </>
-  );
-};
+import { Component } from "react";
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <UserClass name="Sonam Agarwal(Class)" />
+      </>
+    );
+  }
+}
 
 export default About;
