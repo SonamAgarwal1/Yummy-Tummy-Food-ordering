9e68939abf0b1951ef47f9ac99549083829a3a97
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-03 01:09:25 +0530

    reverting changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,11 @@
 import User from "./User";
 import UserClass from "./UserClass";
-import { Component } from "react";
-class About extends Component {
-  constructor(props) {
-    super(props);
-    console.log("Parent Constructor");
-  }
-  componentDidMount() {
-    console.log("Parent componentDidMount");
-  }
-  render() {
-    console.log("Parent Render");
-    return (
-      <>
-        <UserClass name="Sonam Agarwal(Class)" />
-      </>
-    );
-  }
-}
+const About = () => {
+  return (
+    <>
+      <UserClass name="Sonam Agarwal(Class)" />
+    </>
+  );
+};
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,15 +3,9 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log("Child Constructor");
-  }
-
-  componentDidMount() {
-    console.log("Child componentDidMount");
   }
 
   render() {
-    console.log("Child render");
     const { name } = this.props; // destructuring props
     return (
       <div className="user-card">
