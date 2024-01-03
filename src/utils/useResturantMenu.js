00658399c982f9700829205ddd96d3aa68eb37c0
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-04 01:04:18 +0530

    Custom Hook useResturantMenu

---
+++ src/utils/useResturantMenu.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { MENU_API } from "./constant";
+
+const useResturantMenu = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API + resId);
+    const json = await data.json();
+    setResInfo(json);
+  };
+  return resInfo;
+};
+
+export default useResturantMenu;
