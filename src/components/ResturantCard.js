eaf401325e5bc4ff280f7bf88d9abdeabab22dd3
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-17 01:38:15 +0530

    Tailwind css for Resturant card

--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -11,14 +11,14 @@ const ResturantCard = (props) => {
     costForTwo,
   } = resObj?.info;
   return (
-    <div className="res-card">
+    <div className="res-card m-4 p-4 w-[280px] rounded-lg bg-gray-100 hover:bg-gray-200">
       <img
         alt="food-img"
-        className="food-img"
+        className="food-img rounded-lg"
         src={CDN_URL + cloudinaryImageId}
       />
 
-      <h3>{name}</h3>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRatingString} stars</h4>
       <h4>{costForTwo}</h4>
