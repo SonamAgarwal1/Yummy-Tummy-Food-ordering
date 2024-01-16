20aa4060e02a1d2fbd5ad46b6f498568d7b3903c
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-17 01:37:50 +0530

    Tailwind css in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,24 +7,24 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="res-logo">
-      <img className="img-logo" alt="app-logo" src={LOGO_URL} />
-      <div className="nav-bar">
-        <ul>
-          <li>Online Status:{onlineStatus ? "🟢" : "🔴"}</li>
-          <li>
+    <div className="flex justify-between bg-gray-50">
+      <img className="w-20 h-20" alt="app-logo" src={LOGO_URL} />
+      <div className="flex items-center">
+        <ul className="flex m-4 p-4">
+          <li className="px-4">Online Status:{onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             onClick={() => {
               btnName == "Login" ? setBtnName("Logout") : setBtnName("Login");
