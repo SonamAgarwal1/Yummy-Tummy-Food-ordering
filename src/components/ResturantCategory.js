5f4da2f68d4195d76495e7e9d742c1270a0d3362
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-29 20:57:21 +0530

    Adding Accordion for category

--- src/components/ResturantCategory.js
+++ src/components/ResturantCategory.js
@@ -1,6 +1,13 @@
 const ResturantCategory = ({ data }) => {
   console.log(data);
-  return <div>Resturant Category</div>;
+  return (
+    <div>
+      <div className="w-6/12 bg-gray-100 mx-auto my-4 p-2 flex justify-between shadow-lg">
+        <span>{data?.title}</span>
+        <span>▼</span>
+      </div>
+    </div>
+  );
 };
 
 export default ResturantCategory;
