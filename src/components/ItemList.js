cabc02bfc27caa081f336836de909aaf0d6889e8
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-31 21:23:35 +0530

    Item list with images

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constant";
+
 const ItemList = ({ items }) => {
   console.log(items);
   return (
@@ -5,18 +7,23 @@ const ItemList = ({ items }) => {
       {items?.map((item) => (
         <div
           key={item?.card?.info?.id}
-          className="p-2 m-2 border-gray-150 border-b-2 text-left"
+          className="p-2 m-2 border-gray-150 border-b-2 text-left flex justify-between"
         >
-          <div className="py-2">
-            <span>{item?.card?.info?.name}</span>
-            <span>
-              - ₹
-              {item?.card?.info?.price
-                ? item?.card?.info?.price / 100
-                : item?.card?.info?.defaultPrice / 100}
-            </span>
+          <div className="w-9/12">
+            <div className="py-2">
+              <span>{item?.card?.info?.name}</span>
+              <span>
+                - ₹
+                {item?.card?.info?.price
+                  ? item?.card?.info?.price / 100
+                  : item?.card?.info?.defaultPrice / 100}
+              </span>
+            </div>
+            <p className="text-xs">{item?.card?.info?.description}</p>
           </div>
-          <p className="text-xs">{item?.card?.info?.description}</p>
+          <div className="w-3/12">
+            <img src={CDN_URL + item?.card?.info?.imageId} className="w-full" />
+          </div>
         </div>
       ))}
     </div>
