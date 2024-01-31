78fc7b8c1a6993d9ecde93f11f33bacc69cab89c
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-01-31 15:00:36 +0530

    Adding details like price, dishes and description of each item in the accordion

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,12 +3,20 @@ const ItemList = ({ items }) => {
   return (
     <div>
       {items?.map((item) => (
-        <div key={item?.card?.info?.id}>
-          <div>
+        <div
+          key={item?.card?.info?.id}
+          className="p-2 m-2 border-gray-150 border-b-2 text-left"
+        >
+          <div className="py-2">
             <span>{item?.card?.info?.name}</span>
-            <span>₹{item?.card?.info?.price / 100}</span>
+            <span>
+              - ₹
+              {item?.card?.info?.price
+                ? item?.card?.info?.price / 100
+                : item?.card?.info?.defaultPrice / 100}
+            </span>
           </div>
-          <p>{item?.card?.info?.description}</p>
+          <p className="text-xs">{item?.card?.info?.description}</p>
         </div>
       ))}
     </div>
