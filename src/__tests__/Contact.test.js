6e981f59a02983d533c601622e04d01c66194e0c
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-15 15:57:27 +0530

    Contact us component test cases

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -1,9 +1,30 @@
 import Contact from "../components/Contact";
 import { render, screen } from "@testing-library/react";
-import { toBeInTheDocument } from "@testing-library/jest-dom";
+import "@testing-library/jest-dom";
 
 test("Should load contact us component", () => {
   render(<Contact />);
+  //Querying
   const heading = screen.getByRole("heading");
+  //Assertion
   expect(heading).toBeInTheDocument();
+});
+
+test("Should load the button in the component", () => {
+  render(<Contact />);
+  const button = screen.getByText("Submit");
+  expect(button).toBeInTheDocument();
+});
+
+test("Should load the input textbox with the help of placeholder", () => {
+  render(<Contact />);
+  const inputText = screen.getByPlaceholderText("Name");
+  expect(inputText).toBeInTheDocument();
+});
+
+test("Should load 2 input elements", () => {
+  render(<Contact />);
+  const input = screen.getAllByRole("textbox");
+  //console.log(input);
+  expect(input?.length).toBe(2);
 });
