4c85780c5481b917de09b27d31cc588f61bb7a08
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-15 16:10:51 +0530

    Grouping test cases using describe

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -1,30 +1,33 @@
 import Contact from "../components/Contact";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+//describe is to group the test cases together
+describe("To test contact us", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
+    //Querying
+    const heading = screen.getByRole("heading");
+    //Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load contact us component", () => {
-  render(<Contact />);
-  //Querying
-  const heading = screen.getByRole("heading");
-  //Assertion
-  expect(heading).toBeInTheDocument();
-});
+  //it and test are alias and used for same thing.
+  it("Should load the button in the component", () => {
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should load the button in the component", () => {
-  render(<Contact />);
-  const button = screen.getByText("Submit");
-  expect(button).toBeInTheDocument();
-});
+  test("Should load the input textbox with the help of placeholder", () => {
+    render(<Contact />);
+    const inputText = screen.getByPlaceholderText("Name");
+    expect(inputText).toBeInTheDocument();
+  });
 
-test("Should load the input textbox with the help of placeholder", () => {
-  render(<Contact />);
-  const inputText = screen.getByPlaceholderText("Name");
-  expect(inputText).toBeInTheDocument();
-});
-
-test("Should load 2 input elements", () => {
-  render(<Contact />);
-  const input = screen.getAllByRole("textbox");
-  //console.log(input);
-  expect(input?.length).toBe(2);
+  test("Should load 2 input elements", () => {
+    render(<Contact />);
+    const input = screen.getAllByRole("textbox");
+    //console.log(input);
+    expect(input?.length).toBe(2);
+  });
 });
