e56ca78769f635236d5077a65368e956f43a8cc8
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-16 21:55:08 +0530

    Body search test file with mocked data file

---
+++ src/__tests__/Body.test.js
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Body from "../components/Body";
+import MOCK_DATA from "./mocks/resResListMock.json";
+import { act } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+
+it("Should render Body component with ", async () => {
+  await act(async () => {
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    );
+  });
+
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+  const searchInput = screen.getByTestId("searchInput");
+  fireEvent.change(searchInput, { target: { value: "burger" } });
+  fireEvent.click(searchBtn);
+  const cards = screen.getAllByTestId("resCard");
+  //Assertion
+
+  expect(cards?.length).toBe(2);
+});
